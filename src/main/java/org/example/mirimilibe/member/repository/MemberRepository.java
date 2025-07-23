a9de4a58bfa4092fc5f0c04046d263282cb43bc1
LGY010011 <gylm1111pm@gmail.com>
2025-07-24 02:37:08 +0900

    [FEAT] number 기반 조회 추가

--- src/main/java/org/example/mirimilibe/member/repository/MemberRepository.java
+++ src/main/java/org/example/mirimilibe/member/repository/MemberRepository.java
@@ -1,7 +1,12 @@
 package org.example.mirimilibe.member.repository;
 
+import java.util.Optional;
+
 import org.example.mirimilibe.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	Optional<Member> findByNumber(String phoneNumber);
+
 }
