1b09c75d010f962ee17c5a82cdf23e1d6625ec69
LGY010011 <gylm1111pm@gmail.com>
2025-09-03 22:19:12 +0900

    [FEAT] 상세 프로필 설정/수정 명세

--- src/main/java/org/example/mirimilibe/member/controller/MemberController.java
+++ src/main/java/org/example/mirimilibe/member/controller/MemberController.java
@@ -25,9 +25,13 @@ public class MemberController {
 
 	@PostMapping("/profile")
 	@Operation(
-		summary = "프로필 설정",
-		description = "사용자의 군 정보를 설정하는 API입니다. <br>"
-			+ "MiliType은 ENUM 타입으로, 'ARMY, NAVY, AIR_FORCE' 중 하나를 입력해주세요. 해당 필드는 필수로 입력해야 합니다."
+		summary = "프로필 설정/수정",
+		description = "사용자의 군 정보를 설정하는 API입니다. (마이페이지: 현역 사용자의 상세 프로필)<br>"
+			+ "MiliType은 ENUM 타입으로, 'ARMY, NAVY, AIR_FORCE' 중 하나를 입력해주세요. 해당 필드는 필수로 입력해야 합니다. <br>"
+			+ "이미 프로필이 존재하는 경우, 해당 API는 프로필을 수정합니다. <br>"
+			+ "- 기존값이 null이었던 필드에 새로운 값을 입력하는 것은 가능합니다. <br>"
+			+ "- 기존값이 존재하는 필드에 새로운 값을 입력하는 경우 오류를 반환합니다. <br>"
+			+ "마이페이지에서 호출하실 때는 기존 정보들을 불러오신 후, 수정하실 필드만 변경하여 보내주시면 됩니다. <br>"
 	)
 	public ResponseEntity<ApiResponse<String>> updateProfile(@Valid @RequestBody MilitaryInfoReq req, @AuthenticationPrincipal JwtMemberDetail jwtMemberDetail) {
 		memberService.updateMilitaryInfo(req, jwtMemberDetail.getMemberId());
