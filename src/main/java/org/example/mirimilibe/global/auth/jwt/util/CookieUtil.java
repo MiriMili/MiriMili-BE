4ea07454294fb72a16b2df16d357fe52b1233caa
LGY010011 <gylm1111pm@gmail.com>
2025-10-16 19:45:19 +0900

    [FEAT] CookieUtil 추가

---
+++ src/main/java/org/example/mirimilibe/global/auth/jwt/util/CookieUtil.java
@@ -0,0 +1,34 @@
+package org.example.mirimilibe.global.auth.jwt.util;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class CookieUtil {
+	private static final String COOKIE_DOMAIN = "localhost"; // 도메인 설정
+
+	public void setCookie(HttpServletResponse response, String name, String value, int maxAge) {
+		ResponseCookie cookie = ResponseCookie.from(name, value)
+			.path("/")
+			.httpOnly(true)
+			.secure(true)
+			.maxAge(maxAge)
+			.sameSite("None")
+			.domain(COOKIE_DOMAIN)
+			.build();
+
+		response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+	}
+
+	public void deleteCookie(HttpServletResponse response, String name) {
+		Cookie cookie = new Cookie(name, null);
+		cookie.setPath("/");
+		cookie.setHttpOnly(true);
+		cookie.setMaxAge(0);
+		response.addCookie(cookie);
+	}
+}
